953572693f367f250000a41b74c43ce1ede54b4b
outstandinger <1767156609@qq.com>
2020-08-02 16:33:27 +0800

    my_django Girls app,firstcommit

---
+++ blog/templatetags/__init__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------
+   File Name：     __init__.py  
+   Description :  
+   Author :       JHao
+   date：          2017/4/14
+-------------------------------------------------
+   Change Activity:
+                   2017/4/14: 
+-------------------------------------------------
+"""
+__author__ = 'JHao'
+
+import sys
+import importlib
+
+importlib.reload(sys)
+# sys.setdefaultencoding('utf-8')
+
+if __name__ == '__main__':
+    pass
